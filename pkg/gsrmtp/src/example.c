4495b1a291150c4bed9aee9adaf49ee1a7f46638
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2010-07-22 19:31:41 +0000

    /**  * I really would like to have the src directory with the Makevars file to build the jar file.  * But how can I disable the WARNING "Subdirectory 'src' contains no source files."  * other than creating such a dummy c file like this? Or where should I put the java-src?  */

---
+++ pkg/gsrmtp/src/example.c
@@ -0,0 +1,5 @@
+/**
+ * I really would like to have the src directory with the Makevars file to build the jar file.
+ * But how can I disable the WARNING "Subdirectory 'src' contains no source files."
+ * other than creating such a dummy c file like this? Or where should I put the java-src?
+ */
