78297547f509db0c84cb7da8c3c391152a22250e
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2010-08-12 21:01:49 +0000

    graph2latex uses nodeRenderInfo

--- pkg/gsrmtp/R/graph2latex.R
+++ pkg/gsrmtp/R/graph2latex.R
@@ -1,10 +1,11 @@
-graph2latex <- function(graph, package="TikZ") {
+graph2latex <- function(graph, package="TikZ", scale=1) {	
 	tikz <- "\\begin{tikzpicture}"
 	for (node in nodes(graph)) {
 		nodeColor <- ifelse(isRejected(graph, node),"red!80", "green!80")
-		x <- 100
-		y <- 100
-		nodeLine <- paste("\\node (",node,") at (",x,"bp,",y,"bp) [draw,circle,fill=",nodeColor,"] {$",node,"$ \\nodepart{lower} $",format(getAlpha(graph,node), digits=3 ,drop0trailing=TRUE),"$};",sep="")
+		x <- nodeRenderInfo(graph)$nodeX[node]*20*scale
+		y <- nodeRenderInfo(graph)$nodeY[node]*20*scale
+		alpha <- format(getAlpha(graph,node), digits=3 ,drop0trailing=TRUE)
+		nodeLine <- paste("\\node (",node,") at (",x,"bp,",y,"bp) [draw,circle,fill=",nodeColor,"] {$",node,"$ \\nodepart{lower} $",alpha,"$};",sep="")
 		tikz <- paste(tikz, nodeLine,sep="\n")			
 	}
 	# A second loop for the edges is necessary:
@@ -14,7 +15,7 @@ graph2latex <- function(graph, package="TikZ") {
 			for (i in 1:length(edgeL)) {
 				weight <- try(edgeData(graph, names(edgeL[i]), node,"weight"), silent = TRUE)
 				to <- ifelse(class(weight)=="try-error", "auto", "bend left")			
-				weight <- ifelse(edgeL[i]==0, "\\epsilon", edgeL[i])
+				weight <- ifelse(edgeL[i]==0, "\\epsilon", format(edgeL[i], digits=3 ,drop0trailing=TRUE))
 				edgeLine <- paste("\\draw [->] (",node,") to[",to,"] node[fill=blue!20] {",weight,"} (",names(edgeL[i]),");",sep="")
 				tikz <- paste(tikz, edgeLine,sep="\n")
 			}
@@ -29,9 +30,9 @@ createGsrmtpReport <- function(srmtpResult, file="", package="TikZ") {
 	report <- paste(report, "\\usepackage{tikz}", sep="\n")
 	report <- paste(report, "\\usetikzlibrary{snakes,arrows,shapes}", sep="\n")
 	report <- paste(report, "\\begin{document}", sep="\n")
-	for(graph in srmtpResult@graphs) {
-		report <- paste(report, "\\subsection*{Graph}", sep="\n")
-		report <- paste(report, graph2latex(graph), sep="\n")
+	for(i in 1:length(srmtpResult@graphs)) {
+		report <- paste(report, paste("\\subsection*{Graph in Step ",i,"}", sep=""), sep="\n")
+		report <- paste(report, graph2latex(srmtpResult@graphs[[i]]), sep="\n")
 	}
 	report <- paste(report, "\\end{document}", sep="\n")
 	cat(report, file=file)
