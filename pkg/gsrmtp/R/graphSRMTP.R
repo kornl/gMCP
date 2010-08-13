383f24d893e8a9e8c410f697cc01857bdb22545d
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2010-08-13 08:50:58 +0000

    Code for srmtpResult has now its own file.

--- pkg/gsrmtp/R/graphSRMTP.R
+++ pkg/gsrmtp/R/graphSRMTP.R
@@ -1,30 +1,3 @@
-setClass("srmtpResult",		
-		representation(graphs="list",
-				pvalues="numeric")
-)
-
-setMethod("print", "srmtpResult",
-		function(x, ...) {
-			# callNextMethod(x, ...)
-			cat("SRMTP-Result\n")
-			cat("\nP-values:\n")
-			print(x@pvalues)
-			cat("\nInitial graph:\n")
-			print(x@graphs[[1]])
-			if (length(x@graphs)==1) {
-				cat("No hypotheses could be rejected.")
-				return()
-			}
-			cat("\nFinal graph after", length(x@graphs)-1 ,"steps:\n")
-			print(x@graphs[[length(x@graphs)]])
-		})
-
-setMethod("plot", "srmtpResult",
-		function(x, y, ...) {
-			# TODO Show visualization of graph			
-		})
-
-
 ## Graph representation in GSRMTP 
 setClass("graphSRMTP",
 		contains="graphNEL",
