c90f685135eb8115080e0499b7d8c40c6da41898
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2010-08-16 12:06:08 +0000

    r-forge logo correction

--- www/index.php
+++ www/index.php
@@ -23,10 +23,7 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 <body>
 
 <!-- R-Forge Logo -->
-<table border="0" width="100%" cellspacing="0" cellpadding="0">
-<tr><td>
-<a href="/"><img src="<?php echo $themeroot; ?>/images/logo.png" border="0" alt="R-Forge Logo" /> </a> </td> </tr>
-</table>
+<img src="https://r-forge.r-project.org/themes/rforge/imagesrf/logo.png" border="0" alt="R-Forge Logo" />
 
 
 <!-- get project title  -->
