4796b2b906d3c5c13f01872fa6fe28721e76df14
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2010-08-19 07:25:30 +0000

    startGUI guesses now variable name.

--- pkg/gsrmtp/R/startGUIs.R
+++ pkg/gsrmtp/R/startGUIs.R
@@ -1,6 +1,14 @@
 graphGUI <- function(graph="createdGraph") {
 	if (!is.character(graph)) {
-		stop("Please specify the variable name for the graph as character.")
+		warning("Please specify the variable name for the graph as character.")
+		stack <- sys.calls( )
+		stack.fun <- Filter( function(.) .[[1]] == as.name("graphGUI"), stack )
+		graph <- deparse( stack.fun[[1]][[2]] )
+		warning(paste("We guess you wanted to use graphGUI(\"",graph,"\")",sep=""))
+		# TODO Can we find out whether a call is something like
+		# graphGUI(createGraphFromBretzEtAl())?
+		# Because in this case we should simply stop with an error.
+		# Does "exist" solve this problem? Or a confirmation during saving?
 	}
-	.jcall("org/mutoss/gui/CreateGraphGUI", method="startGUI", graph)
+	.jnew("org/mutoss/gui/CreateGraphGUI", graph)	
 }
