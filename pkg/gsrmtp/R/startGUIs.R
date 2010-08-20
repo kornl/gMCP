436d33f53e598e357ade449e8a1d5c8b07c855ac
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2010-08-20 09:14:02 +0000

    We use make.names to take care that we can assign the graph to the given character string.

--- pkg/gsrmtp/R/startGUIs.R
+++ pkg/gsrmtp/R/startGUIs.R
@@ -3,12 +3,8 @@ graphGUI <- function(graph="createdGraph") {
 		warning("Please specify the variable name for the graph as character.")
 		stack <- sys.calls( )
 		stack.fun <- Filter( function(.) .[[1]] == as.name("graphGUI"), stack )
-		graph <- deparse( stack.fun[[1]][[2]] )
-		warning(paste("We guess you wanted to use graphGUI(\"",graph,"\")",sep=""))
-		# TODO Can we find out whether a call is something like
-		# graphGUI(createGraphFromBretzEtAl())?
-		# Because in this case we should simply stop with an error.
-		# Does "exist" solve this problem? Or a confirmation during saving?
+		graph <- make.names(deparse( stack.fun[[1]][[2]] ))
+		warning(paste("We guess you wanted to use graphGUI(\"",graph,"\")",sep=""))		
 	}
 	.jnew("org/mutoss/gui/CreateGraphGUI", graph)	
 }
