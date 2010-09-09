9c90a6794e6630ae6105446411767631777376b3
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2010-09-09 18:10:13 +0000

    Changed annotations for Java 5.

--- pkg/gMCP/inst/java-src/org/mutoss/gui/CreateGraphGUI.java
+++ pkg/gMCP/inst/java-src/org/mutoss/gui/CreateGraphGUI.java
@@ -59,18 +59,11 @@ public class CreateGraphGUI extends JFrame implements WindowListener {
 		new CreateGraphGUI("graph", true, 10);
 	}
 
-	@Override
 	public void windowActivated(WindowEvent e) {}
-	@Override
 	public void windowClosed(WindowEvent e) {}
-	@Override
 	public void windowClosing(WindowEvent e) {}
-	@Override
 	public void windowDeactivated(WindowEvent e) {}
-	@Override
 	public void windowDeiconified(WindowEvent e) {}
-	@Override
 	public void windowIconified(WindowEvent e) {}
-	@Override
 	public void windowOpened(WindowEvent e) {}
 }
