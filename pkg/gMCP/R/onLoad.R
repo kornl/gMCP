150b4dff95398173e35251d9d6bf162d047b5bd7
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2010-09-09 17:36:12 +0000

    onLoad tries to add JRI jars to classpath.

--- pkg/gMCP/R/onLoad.R
+++ pkg/gMCP/R/onLoad.R
@@ -1,5 +1,7 @@
 .onLoad <- function(libname, pkgname) {
 	.jpackage(pkgname)
+	# Does this work?
+	.jpackage(rJava)
 	## we supply our own JavaGD class
 	#.setenv <- if (exists("Sys.setenv")) Sys.setenv else Sys.putenv
 	#.setenv("JAVAGD_CLASS_NAME"="org/mutoss/gui/JavaGD")  
