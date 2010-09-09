5ec4686d9a95ab040c4e59e0b91ca1869c850281
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2010-09-09 17:49:05 +0000

    We don't deploy the JRI jars any more.

--- pkg/gMCP/R/onLoad.R
+++ pkg/gMCP/R/onLoad.R
@@ -1,7 +1,14 @@
 .onLoad <- function(libname, pkgname) {
 	.jpackage(pkgname)
 	# Does this work?
-	.jpackage(rJava)
+    classes <- system.file("jri", package = "rJava", lib.loc = NULL)
+	if (nchar(classes)) {
+		.jaddClassPath(classes)
+		jars <- grep(".*\\.jar", list.files(classes, full.names = TRUE), TRUE, value = TRUE)
+		if (length(jars)) { 
+			.jaddClassPath(jars)
+		}		
+	}	
 	## we supply our own JavaGD class
 	#.setenv <- if (exists("Sys.setenv")) Sys.setenv else Sys.putenv
 	#.setenv("JAVAGD_CLASS_NAME"="org/mutoss/gui/JavaGD")  
