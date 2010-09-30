e71278614519cdbab22a266bf4040d0c0e2eef35
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2010-09-30 23:34:19 +0000

    Deducer support.

--- pkg/gMCP/R/onLoad.R
+++ pkg/gMCP/R/onLoad.R
@@ -12,4 +12,15 @@
 	## we supply our own JavaGD class
 	#.setenv <- if (exists("Sys.setenv")) Sys.setenv else Sys.putenv
 	#.setenv("JAVAGD_CLASS_NAME"="org/mutoss/gui/JavaGD")  
+	
+	# Optional Deducer integration:
+	if(exists(".deducer")) {
+		if (!is.null(.deducer)) {
+			deducer.addMenuItem("Multiple Test Graph",,"graphGUI()","Analysis")
+			if(.jgr){
+				jgr.addMenuSeparator("Analysis")
+				jgr.addMenuItem("Analysis","Multiple Test Graph","graphGUI()")
+			}
+		}
+	}
 }  
