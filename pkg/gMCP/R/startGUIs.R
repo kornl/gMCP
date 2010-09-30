4cc537de1e073829b0edc6d00edd17c882c97d70
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2010-09-30 11:04:21 +0000

    graphGUI can now also be called directly on objects of class graphSRMTP. Also updated TODO, NEWS, INSTALL.

--- pkg/gMCP/R/startGUIs.R
+++ pkg/gMCP/R/startGUIs.R
@@ -1,10 +1,21 @@
 graphGUI <- function(graph="createdGraph", grid=1, debug=FALSE) {
 	if (!is.character(graph)) {
-		warning("Please specify the variable name for the graph as character.")
-		stack <- sys.calls( )
-		stack.fun <- Filter( function(.) .[[1]] == as.name("graphGUI"), stack )
-		graph <- make.names(deparse( stack.fun[[1]][[2]] ))
-		warning(paste("We guess you wanted to use graphGUI(\"",graph,"\")",sep=""))		
+		if ("graphSRMTP" %in% class(graph)) {
+			newGraphName <- "createdGraph"
+			i <- 2
+			while(exists(newGraphName, envir=globalenv())) {
+				newGraphName <- paste("createdGraph", i, sep="")
+				i <- i + 1
+			}
+			assign(newGraphName, graph, envir=globalenv())
+			graph <- newGraphName
+		} else {
+			warning("Please specify the variable name for the graph as character.")
+			stack <- sys.calls( )
+			stack.fun <- Filter( function(.) .[[1]] == as.name("graphGUI"), stack )
+			graph <- make.names(deparse( stack.fun[[1]][[2]] ))
+			warning(paste("We guess you wanted to use graphGUI(\"",graph,"\")",sep=""))
+		}
 	}
 	invisible(.jnew("org/mutoss/gui/CreateGraphGUI", make.names(graph), debug, grid))	
 }
