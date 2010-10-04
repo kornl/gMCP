0865a3493cd2eb85e33a2200da0dd248c2fb08db
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2010-10-04 10:02:29 +0000

    More infos in email header for bug reports.

--- pkg/gMCP/inst/java-src/org/mutoss/gui/ErrorDialogSGTK.java
+++ pkg/gMCP/inst/java-src/org/mutoss/gui/ErrorDialogSGTK.java
@@ -125,7 +125,7 @@ public class ErrorDialogSGTK extends ErrorDialog {
 
     protected Hashtable<String, String> getInfoTable() {
     	Hashtable<String, String> table = super.getInfoTable();
-    	table.put("Subject", "gMCP bug report");
+    	table.put("Subject", "gMCP bug report from "+System.getProperty("user.name", "<unknown user name>")+" on "+System.getProperty("os.name", "<unknown OS>"));
     	return table;
     }
   
