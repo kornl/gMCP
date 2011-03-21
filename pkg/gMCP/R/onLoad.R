38ca64ebc278d1bc07b04d02cffda06b4044c9f9
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2011-03-21 21:36:51 +0000

    If we have a rJava version < 0.8-3 load JRIEngine.jar and REngine.jar from package gMCP.

--- pkg/gMCP/R/onLoad.R
+++ pkg/gMCP/R/onLoad.R
@@ -1,6 +1,6 @@
 .onLoad <- function(libname, pkgname) {
 	.jpackage(pkgname)
-	# Does this work?
+
 	classes <- system.file("jri", package = "rJava", lib.loc = NULL)
 	if (nchar(classes)) {
 		.jaddClassPath(classes)
@@ -8,7 +8,20 @@
 		if (length(jars)) { 
 			.jaddClassPath(jars)
 		}		
-	}	
+	}
+	
+	# If we have a rJava version < 0.8-3 load JRIEngine.jar and REngine.jar
+	if (sessionInfo()$otherPkgs$rJava$Version<"0.8-3") {
+		classes <- system.file("R28", package = "gMCP", lib.loc = NULL)
+		if (nchar(classes)) {
+			.jaddClassPath(classes)
+			jars <- grep(".*\\.jar", list.files(classes, full.names = TRUE), TRUE, value = TRUE)
+			if (length(jars)) { 
+				.jaddClassPath(jars)
+			}		
+		}
+	}
+	
 	## we supply our own JavaGD class
 	#.setenv <- if (exists("Sys.setenv")) Sys.setenv else Sys.putenv
 	#.setenv("JAVAGD_CLASS_NAME"="org/mutoss/gui/JavaGD")  
