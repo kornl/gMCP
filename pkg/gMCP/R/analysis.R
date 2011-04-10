c3d0067941e7dce4f15f48cf61b921b5ac933022
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2011-04-10 22:05:27 +0000

    New function graphAnalysis

---
+++ pkg/gMCP/R/analysis.R
@@ -0,0 +1,23 @@
+graphAnalysis <- function(graph, file="") {
+	result <- ""
+	accessible <- acc(graph, nodes(graph))
+	for (i in names(accessible)) {
+		missingNodes <- c()
+		for (n in nodes(graph)) {
+			if (!(n %in% names(accessible[[i]])) && n!=i) {
+				missingNodes <- c(missingNodes, n)
+			}			
+		}
+		if (length(missingNodes)!=0) {
+			result <- paste(result, "The following nodes are not accessible from '",i,"': ",
+					paste(missingNodes, collapse =", "), "\n", sep="")
+		}
+	}
+	if (result=="") {
+		result <- "Each node is accessible from each other node.\n"
+	} else {
+		
+	}
+	cat(result)
+	return(invisible(result))
+}
