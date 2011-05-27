26a3ad8a795fcef568c96cfe417427db6ade4d5c
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2011-05-27 15:51:27 +0000

    cranberries etc.

--- pkg/gMCP/inst/unitTests/runit.misc.R
+++ pkg/gMCP/inst/unitTests/runit.misc.R
@@ -6,3 +6,13 @@ test.parseEpsPolynom <- function() {
 	checkEquals(c(5, 3, 2), 
 			gMCP:::parseEpsPolynom("2*epsilon*epsilon+5+3*epsilon"))
 }
+
+test.matrix2graph <- function() {
+	m<-rbind(H1=c("0",         "0",         "0.5",         "0.5"),
+			H2=c("0",         "0",         "0.5",         "0.5"),
+			H3=c("\\epsilon", "0",         "0",           "1-\\epsilon"),
+			H4=c("0",         "\\epsilon", "1-\\epsilon", "0"))
+	colnames(m) <- rownames(m)
+	m2 <- graph2matrix(matrix2graph(m))
+	checkEquals(m, m2)
+}
