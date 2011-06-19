a26e09cfb38ed01966cde64a5e1b53c9085d073d
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2011-06-19 13:31:37 +0000

    Function requireLibrary

--- pkg/gMCP/R/misc.R
+++ pkg/gMCP/R/misc.R
@@ -78,3 +78,15 @@ bdiagNA <- function(...) {
 	}	
 	return(M)
 }
+
+requireLibrary <- function(package) {
+	if(!require(package, character.only=TRUE)) {
+		answer <- readline(paste("Package ",package," is required - should we install it?", sep=""))
+		if (substr(answer, 1, 1) %in% c("y","Y")) {
+			install.packages(package)
+			require(package, character.only=TRUE)
+		} else {
+			stop(paste("Required package ",package," should not be installed.", sep=""))
+		}
+	}
+}
