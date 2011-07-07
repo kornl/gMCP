89395ae0305ffbeb71c8384853103c09da1af6b7
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2011-07-07 09:15:12 +0000

    substituteEps returns numeric matrix if it contains no more variables.

--- pkg/gMCP/R/subVariables.R
+++ pkg/gMCP/R/subVariables.R
@@ -1,6 +1,8 @@
 substituteEps <- function(graph, eps=10^(-4)) {
 	if (is.numeric(graph@m)) return(graph)
-	m <- matrix(gsub("\\\\epsilon", eps, graph@m),nrow=length(nodes(graph)))
+	m <- matrix(gsub("\\\\epsilon", eps, graph@m), nrow=length(nodes(graph)))
+	m2 <- matrix(sapply(graph@m, function(x) {result <- try(eval(parse(text=x)), silent=TRUE); ifelse(class(result)=="try-error",NA,result)}), nrow=length(nodes(graph)))
+	if (all(is.na(m)==is.na(m2))) m <- m2
 	rownames(m) <- colnames(m) <- nodes(graph)
 	graph@m <- m
 	return(graph)
