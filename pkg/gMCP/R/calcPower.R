446d35ab5ecab1e47ae092025f27c83f7194c967
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2011-07-11 13:35:46 +0000

    Code and docs from Bjoern

---
+++ pkg/gMCP/R/calcPower.R
@@ -0,0 +1,18 @@
+extractPower <- function(x){
+  pow <- colMeans(x)
+  avgPow <- sum(x)/nrow(x)
+  atleast1 <- mean(rowSums(x)>0)
+  list(LocalPower = pow, ExpRejections = avgPow,
+       PowAtlst1 = atleast1)
+}
+
+calcPower <- function(alphas, G, mean = rep(0, nrow(sigma)),
+                      sigma = diag(length(mean)),corr = NULL,
+                      nSim = 10000, seed = 4711, type = c("quasirandom", "pseudorandom")){
+  type <- match.arg(type)
+  sims <- rqmvnorm(nSim, mean = mean, sigma = sigma, corr = corr,
+                   seed = seed, type = type)
+  pvals <- pnorm(sims, lower.tail = FALSE)
+  out <- graphTest(pvals, alphas, G)
+  extractPower(out)
+}
