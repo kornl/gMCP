6b1b413bf5df5b42620cee2955b3b5d5422ec405
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2011-11-15 16:17:15 +0000

    verbosity

--- pkg/gMCP/R/gMCP.R
+++ pkg/gMCP/R/gMCP.R
@@ -88,6 +88,7 @@ gMCP <- function(graph, pvalues, test, correlation, alpha=0.05,
 			allSubsets <- permutations(length(getNodes(graph2)))
 			result <- cbind(allSubsets, 0)
 			weights <- generateWeights(graph2@m, getWeights(graph2))[,(n+1:n)]
+			if (verbose) explanation <- rep("", dim(allSubset)[1])
 			for (i in 1:dim(allSubset)[1]) {
 				subset <- allSubsets[i,]
 				if(!all(subset==0)) {
@@ -96,6 +97,7 @@ gMCP <- function(graph, pvalues, test, correlation, alpha=0.05,
 					for (j in J) {
 						if (pvalues[j]<=alpha*sum(weights[i, Jj])) {
 							result[i, n+1] <- 1
+							if (verbose) 
 						}
 					}					
 				} 
