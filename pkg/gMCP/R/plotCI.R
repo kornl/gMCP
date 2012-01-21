ea7681bf3c00b3ee53e83847b63d2a7fa7fbf16a
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2012-01-21 10:03:48 +0000

    Fix for plotCI if all finite points are the same.

--- pkg/gMCP/R/plotCI.R
+++ pkg/gMCP/R/plotCI.R
@@ -8,7 +8,6 @@ plotSimCI <- function(ci) {
 	lower=ci[,1] 
 	upper=ci[,3]
 	
-	alternative="two.sided"#=c("two.sided","less","greater") TODO Check this.
 	lines=NULL
 	lineslty=2
 	lineslwd=1
@@ -18,7 +17,7 @@ plotSimCI <- function(ci) {
 	CIcex=1
 	CIcol="black"
 	CIlength=NULL
-
+	
 	old.par <- par(no.readonly=TRUE)
 	
 	aargs <- list()
@@ -65,112 +64,63 @@ plotSimCI <- function(ci) {
 	
 	if(is.null(lower) & is.null(upper)){warning("No confidence limits specified: arguments lower and upper are both empty!")}
 	
-# define the plot range in 
-# dependence of the alternative:
+# define the plot range
 	
-	switch(alternative,
-			"two.sided"={
-				allpoints <- c(lower, estimate, upper)
-				if(all(!is.finite(allpoints))){stop("Arguments estimate, lower and upper contain only infinity or missing values!")}
-				allexist<-allpoints[is.finite(allpoints)]
-				lplot <- min(c(allexist, lines))
-				uplot <- max(c(allexist, lines))
-			},
-			
-			"less"={						
-				allpoints<-c(estimate, upper)
-				if(all(!is.finite(allpoints))){stop("Arguments estimate and upper contain only infinity or missing values!")}
-				allexist<-allpoints[is.finite(allpoints)]
-				lplot <- min(c(lines, allexist)) 
-				uplot <- max(c(allexist, lines))
-			},
-			
-			"greater"={
-				allpoints<-c(lower, estimate)
-				if(all(!is.finite(allpoints))){stop("Arguments estimate and lower contain only infinity or missing values!")}
-				allexist<-allpoints[is.finite(allpoints)]
-				lplot <- min(c(lines, allexist)) 
-				uplot <- max(c(lines, allexist))
-				
-			})
+	allpoints <- c(lower, estimate, upper)
+	if(all(!is.finite(allpoints))){stop("Arguments estimate, lower and upper contain only infinity or missing values!")}
+	allexist<-allpoints[is.finite(allpoints)]
+	lplot <- min(c(allexist, lines))
+	uplot <- max(c(allexist, lines))
 	
 # Define the final plot ranges:
 	
-	llplot <- lplot - 0.1*abs(lplot-uplot)
-	uuplot <- uplot + 0.1*abs(lplot-uplot)
+	if ((lplot-uplot)!=0) {
+		llplot <- lplot - 0.1*abs(lplot-uplot)
+		uuplot <- uplot + 0.1*abs(lplot-uplot)
+	} else {
+		llplot <- lplot -1.2
+		uuplot <- uplot +1.2
+	}
 	
 # define the type of interval drawn,
 # appropriate for unbounded CI
 	
-	switch(alternative,			
-			"two.sided"={				
-				if(is.null(lower)){
-					lower<-rep(llplot,k); code<-rep(2,k)
-					warning("No lower limits specified!")
-				}
-				else{
-					if(is.null(upper)){
-						upper<-rep(uuplot,k); code<-rep(1,k)
-						warning("No upper limits specified!")
-					} else {
-						code<-rep(3,k)
-						
-						infl<-!is.finite(lower)
-						lower[infl]<-llplot
-						code[infl]<-2
-						
-						infu<-!is.finite(upper)
-						upper[infu]<-uuplot
-						code[infu]<-1
-						
-						infts <- infl&infu
-						code[infts]<-0
-					}}
-			},
-			
-			"less"={
-				code<-rep(2,k)
-				
-				if(is.null(upper)){
-					upper<-rep(uuplot,k); code<-rep(0,k)
-					warning("No upper limits specified although alternative='less'!")
-				}
-				
-				infu<-!is.finite(upper)
-				upper[infu]<-uuplot
-				code[infu]<-0
-				
-			},
-			
-			"greater"={
-				code<-rep(1,k)
-				
-				if(is.null(lower)){
-					lower<-rep(llplot,k); code<-rep(0,k)
-					warning("No lower limits specified although alternative='greater'!")
-				}
-				
-				infl<-!is.finite(lower)
-				lower[infl]<-llplot
-				code[infl]<-0
-			})
-	
+	if(is.null(lower)){
+		lower<-rep(llplot,k); code<-rep(2,k)
+		warning("No lower limits specified!")
+	} else if(is.null(upper)){
+		upper<-rep(uuplot,k); code<-rep(1,k)
+		warning("No upper limits specified!")
+	} else {
+		code<-rep(3,k)
+		
+		infl<-!is.finite(lower)
+		lower[infl]<-llplot
+		code[infl]<-2
+		
+		infu<-!is.finite(upper)
+		upper[infu]<-uuplot
+		code[infu]<-1
+		
+		infts <- infl&infu
+		code[infts]<-0
+	}
 	
 # Define the defaults for main, sub, ylab, xlab:
 	
-	aargs$main<-"" 
-	aargs$sub<-""
-	aargs$ylab<-"" 
-	aargs$xlab<-""
+	aargs$main <- "" 
+	aargs$sub  <- ""
+	aargs$ylab <- "" 
+	aargs$xlab <- ""
 	
 # Box arguments
 	
-	BTY<-"o"
+	BTY <-"o"
 	
 	plot.new()
 	
 	# adjust margin under the x axis according to length of comparison names
-	ywidth<- 1.5 * max(strwidth(compn, units = "inches", cex = par("cex.axis"))) 
+	ywidth <- 1.5 * max(strwidth(compn, units = "inches", cex = par("cex.axis"))) 
 	
 	if (mymai[2] < ywidth) mymai[2] <- ywidth
 	
@@ -178,14 +128,14 @@ plotSimCI <- function(ci) {
 	
 	rnum<-rev(num)
 	
-	aargs$y<-rnum
-	aargs$x<-estimate
-	aargs$axes<-FALSE
+	aargs$y <- rnum
+	aargs$x <- estimate
+	aargs$axes <- FALSE
 	
-	aargs$xlim<-c(llplot, uuplot)
-	aargs$type<-"p"
-	aargs$pch<-16
-	aargs$cex<-CIcex
+	aargs$xlim <- c(llplot, uuplot)
+	aargs$type <- "p"
+	aargs$pch <- 16
+	aargs$cex <- CIcex
 	
 	do.call("plot", aargs)
 	
@@ -199,33 +149,11 @@ plotSimCI <- function(ci) {
 	
 	if(is.null(CIlength)) arrlength<-1/(k*2)
 	
-	switch(alternative,
-			
-			"two.sided"={
-				for(i in 1:length(num))	{
-					
-					arrows(y0=rnum[i], y1=rnum[i], x0=lower[i], x1=upper[i],
-							length = arrlength, angle = 90, code = code[i],
-							col = CIcol[i], lty = CIlty[i], lwd = CIlwd[i])
-					
-				}
-			},
-			
-			"less"={
-				for(i in 1:length(num))	{
-					arrows(y0=rnum[i], y1=rnum[i], x0=llplot, x1=upper[i],
-							length = arrlength, angle = 90, code = code[i],
-							col = CIcol[i], lty = CIlty[i], lwd = CIlwd[i])
-				}
-			},
-			
-			"greater"={
-				for(i in 1:length(num))	{
-					arrows(y0=rnum[i], y1=rnum[i], x0=lower[i], x1=uuplot,
-							length = arrlength, angle = 90, code = code[i],
-							col = CIcol[i], lty = CIlty[i], lwd = CIlwd[i])
-				}
-			})	
+	for(i in 1:length(num))	{		
+		arrows(y0=rnum[i], y1=rnum[i], x0=lower[i], x1=upper[i],
+				length = arrlength, angle = 90, code = code[i],
+				col = CIcol[i], lty = CIlty[i], lwd = CIlwd[i])
+	}
 	
 	par(old.par)	
 }
