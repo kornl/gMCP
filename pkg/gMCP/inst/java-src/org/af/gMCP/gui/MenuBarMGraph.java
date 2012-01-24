ca7b786d6e790d31b4f6ff7d1a9f92585ee13a81
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2012-01-24 19:41:29 +0000

    LaTeX code fragment is copied to system clipboard

--- pkg/gMCP/inst/java-src/org/af/gMCP/gui/MenuBarMGraph.java
+++ pkg/gMCP/inst/java-src/org/af/gMCP/gui/MenuBarMGraph.java
@@ -1,5 +1,7 @@
 package org.af.gMCP.gui;
 
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -541,7 +543,9 @@ public class MenuBarMGraph extends JMenuBar implements ActionListener {
 	
 	
 	public void showLaTeXGraph() {
-		new TextFileViewer(control.getGraphGUI(), "LaTeX code", control.getNL().getLaTeX(), 
+		String latexCode = control.getNL().getLaTeX();
+		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(new StringSelection(latexCode), null);		
+		new TextFileViewer(control.getGraphGUI(), "LaTeX code", latexCode, 
 				"Also add the following lines to the header of your LaTeX file:\n\\usepackage{tikz}\n\\usetikzlibrary{snakes,arrows,shapes}");
 	}
 	
