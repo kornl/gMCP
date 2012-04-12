66efa3da69f03224b7658ce832bc73fb1be83bdd
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2012-04-12 20:03:45 +0000

    test file

--- test/R/calcPower.R
+++ test/R/calcPower.R
@@ -5,4 +5,7 @@ corMat <- rbind(c(1, 0.5, 0.5, 0.5/2),
 		c(0.5,0.5/2,1,0.5),
 		c(0.5/2,0.5,0.5,1))
 theta <- c(3, 0, 0, 0)
-calcPower(weights, alpha=0.025, G, theta, corMat, cr=corMat, seed = 4711, nSim = 1000)
+result2 <- calcPower(weights, alpha=0.025, G, theta, corMat, nSim = 10000)
+result2
+result1 <- calcPower(weights, alpha=0.025, G, theta, corMat, cr=corMat, nSim = 1000)
+result1
