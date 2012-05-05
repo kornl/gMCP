0686dbacf5d016144f8d1e6062c0a050cd824bb3
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2012-05-05 13:03:59 +0000

    Output of user defined functions - but labels have yet to be set by the GUI.

--- pkg/gMCP/R/calcPower.R
+++ pkg/gMCP/R/calcPower.R
@@ -44,25 +44,41 @@ calcMultiPower <- function(weights, alpha, G, muL, sigmaL, nL,
 		sigma = diag(length(muL[[1]])), cr = NULL,
 		nSim = 10000, seed = 4711, type = c("quasirandom", "pseudorandom"),
 		f=list()) {
-	l <- list()
+	meanL <- list()
 	for (mu in muL) {
 		for (s in sigmaL) {
 			for (n in nL) {
 				newSetting <- mu*sqrt(n)/s
 				attr(newSetting, "label") <- paste("mu: ",paste(mu,collapse=","),", sigma: ",paste(s,collapse=","),", n: ",paste(n,collapse=","),sep="")
-				l[[length(l)+1]] <- newSetting 
+				meanL[[length(meanL)+1]] <- newSetting 
 			}
 		}
 	}
 	sResult <- ""
-	resultL <- calcPower(weights, alpha, G, mean = l, sigma, cr, nSim, seed, type, f)
+	g <- matrix2graph(G)
+	g <- setWeights(g, weights)
+	sResult <- paste(sResult, "Graph:",paste(capture.output(print(g)), collapse="\n"), sep="\n")
+	resultL <- calcPower(weights, alpha, G, mean = meanL, sigma, cr, nSim, seed, type, f)
 	for(result in resultL) {
 		label <- attr(result, "label")
-		sResult <- paste(sResult, label, sep="\n")			
-		sResult <- paste(sResult, "Local Power:\n",paste(capture.output(cat(result$LocalPower)), collapse="\n"), sep="\n")
+		title <- paste("Setting:",label)		
+		sResult <- paste(sResult, title, paste(rep("=", nchar(title)),collapse=""), sep="\n")			
+		sResult <- paste(sResult, "Local Power:",paste(capture.output(print(result$LocalPower)), collapse="\n"), sep="\n")
+		sResult <- paste(sResult, "\nExpected number of rejections:", result$ExpRejections, sep="\n")
+		sResult <- paste(sResult, "Prob. to reject at least one hyp.:", result$PowAtlst1, sep="\n")
+		sResult <- paste(sResult, "Prob. to reject all hypotheses:", result$RejectAll, sep="\n")
+		if (length(result)>4) {
+			for (i in 5:length(result)) {
+				#TODO pF <- attr(result, "label")
+				pF <- attr(result[i], "label")
+				if (is.null(pF)) pF <- names(result)[i]
+				sResult <- paste(sResult, paste(pF,":",sep=""), result[i], sep="\n")
+			}
+		}
+		sResult <- paste(sResult, "\n", sep="\n")		
 	}
 	return(sResult)
 }
 
-x <- calcMultiPower(weights=BonferroniHolm(3)@weights, alpha=0.05, G=BonferroniHolm(3)@m, muL=list(c(0,0,0),c(10,10,10),c(10,20,30)), sigmaL=list(c(1,1,1)), nL=list(c(10,10,10),c(20,20,20)))
-cat(x)
+#x <- calcMultiPower(weights=BonferroniHolm(3)@weights, alpha=0.05, G=BonferroniHolm(3)@m, muL=list(c(0,0,0),c(10,10,10),c(10,20,30)), sigmaL=list(c(1,1,1)), nL=list(c(10,10,10),c(20,20,20)), f=list(p1=function(x){x[1]&&x[2]}))
+#cat(x)
