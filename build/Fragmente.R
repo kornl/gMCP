ff9ec29e41b8d1fd3969545aa9edeeb6718c58bc
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2012-06-20 17:21:34 +0000

    Replaced graph analysis with our own methods.

--- build/Fragmente.R
+++ build/Fragmente.R
@@ -4,3 +4,37 @@ getStr <- function(x) {
 	if (length(w)==0) return(as.character(x))
 	return(paste(w[1]%%100,w[1]%/%100,sep="/"))
 }
+
+
+options(warn=-1)
+if (require("graph", quietly=TRUE)) {
+	options(warn=0)
+	hnodes <- getNodes(graph)
+	graph <- as(new("graphAM", adjMat=graph@m, edgemode="directed"), "graphNEL")
+	accessible <- acc(graph, hnodes)
+	for (i in names(accessible)) {
+		missingNodes <- c()
+		for (n in nodes(graph)) {
+			if (!(n %in% names(accessible[[i]])) && n!=i) {
+				missingNodes <- c(missingNodes, n)
+			}			
+		}
+		if (length(missingNodes)!=0) {
+			result <- paste(result, "The following nodes are not accessible from '",i,"': ",
+					paste(missingNodes, collapse =", "), "\n", sep="")
+		}
+	}
+	if (result=="") {
+		result <- "Each node is accessible from each other node.\n"
+	} else {
+		
+	}
+} else {
+	options(warn=0)
+	result <- paste("Install package \"graph\" for graph analysis:",
+			"",
+			"source(\"http://www.bioconductor.org/biocLite.R\")",
+			"biocLite(\"graph\")",
+			"",
+			"and restart R.", sep="\n");
+}
--- pkg/gMCP/R/analysis.R
+++ pkg/gMCP/R/analysis.R
@@ -1,37 +1,5 @@
 graphAnalysis <- function(graph, file="") {
-	result <- ""
-	options(warn=-1)
-	if (require("graph", quietly=TRUE)) {
-		options(warn=0)
-		hnodes <- getNodes(graph)
-		graph <- as(new("graphAM", adjMat=graph@m, edgemode="directed"), "graphNEL")
-		accessible <- acc(graph, hnodes)
-		for (i in names(accessible)) {
-			missingNodes <- c()
-			for (n in nodes(graph)) {
-				if (!(n %in% names(accessible[[i]])) && n!=i) {
-					missingNodes <- c(missingNodes, n)
-				}			
-			}
-			if (length(missingNodes)!=0) {
-				result <- paste(result, "The following nodes are not accessible from '",i,"': ",
-						paste(missingNodes, collapse =", "), "\n", sep="")
-			}
-		}
-		if (result=="") {
-			result <- "Each node is accessible from each other node.\n"
-		} else {
-			
-		}
-	} else {
-		options(warn=0)
-		result <- paste("Install package \"graph\" for graph analysis:",
-				"",
-				"source(\"http://www.bioconductor.org/biocLite.R\")",
-				"biocLite(\"graph\")",
-				"",
-				"and restart R.", sep="\n");
-	}
+	result <- checkOptimal(graph, verbose=FALSE)
 	cat(result, file=file)
 	return(invisible(result))
 }
