1dd85a72a5fb8798159a46644e4da66ca8cbeec7
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2012-06-21 21:30:05 +0000

    TestSuite prints the results of the JUnit tests.

--- pkg/gMCP/inst/java-src/tests/TestSuite.java
+++ pkg/gMCP/inst/java-src/tests/TestSuite.java
@@ -1,7 +1,25 @@
 package tests;
 
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
 public class TestSuite {
+	
+	public static void main(String[] args) {
+		Result result = org.junit.runner.JUnitCore.runClasses(getClasses());
+		System.out.println(getResultString(result));
+	}
+	
     public static Class[] getClasses() {
     	return new Class[] { RControlTest.class };
+    }
+    
+    public static String getResultString(Result result) {
+    	String s = "There were "+result.getFailureCount()+" failures ("+result.getRunCount()+" tests; runtime "+result.getRunTime()/1000+" sec):\n\n";
+    	for (Failure f : result.getFailures()) {
+    		s += "Failure in "+f.getDescription().getClassName()+"."+f.getDescription().getMethodName()+"()\n";
+    		s += "Message: "+f.getMessage()+"\n\nTrace: "+f.getTrace()+"\n\n";
+    	}
+    	return s;
     }
 }
