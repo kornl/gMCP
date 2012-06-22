cbb865014adef8563efe91847d8d32d25dd21f3f
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2012-06-22 13:05:53 +0000

    test skeletons

---
+++ pkg/gMCP/inst/java-src/tests/EdgeTest.java
@@ -0,0 +1,34 @@
+package tests;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.af.gMCP.gui.RControl;
+import org.af.gMCP.gui.graph.NetList;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class EdgeTest {
+
+    protected RControl ctrl;
+    protected NetList nl;
+
+    @Before
+    public void setUp() {
+    	ctrl = RControl.getRControl(true);
+    }
+
+    @After
+    public void cleanUp() {
+    }
+
+    @Test
+    public void testLoadSave() {
+    }
+    
+  }
+	  
+
