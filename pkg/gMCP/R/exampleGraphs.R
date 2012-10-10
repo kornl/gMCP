59c0357414c68d2788a090d66b7d23de679c7ad5
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2012-10-10 06:25:36 +0000

    description for graph from Ferber.

--- pkg/gMCP/R/exampleGraphs.R
+++ pkg/gMCP/R/exampleGraphs.R
@@ -618,6 +618,9 @@ FerberTimeDose2011 <- function(times, doses, w="\\nu") {
 	rownames(m) <- colnames(m) <- hnodes 
 	# Graph creation
 	graph <- new("graphMCP", m=m, weights=c(rep(0, times*doses-1), 1))
+	attr(graph, "description") <- paste("Second graph from Ferber et al. 2011",
+			"",
+			"Literature: G. Ferber, L. Staner and P. Boeijinga (2011): Structured multiplicity and confirmatory statistical analyses in pharmacodynamic studies using the quantitative electroencephalogram, Journal of neuroscience methods, Volume 201, Issue 1, Pages 204-212.", sep="\n")
 	# Placing nodes and converting to numeric
 	graph <- placeNodes(gMCP:::parse2numeric(graph), times, doses)
 	return(graph)
