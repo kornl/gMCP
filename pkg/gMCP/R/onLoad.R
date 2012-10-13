2b4e92be912d24903e2ca6cf57ba6d6186bd184a
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2012-10-13 20:26:56 +0000

    New goal: Version: 0.8-1 (we depend on CommonJavaJars>=1.0-3)

--- pkg/gMCP/R/onLoad.R
+++ pkg/gMCP/R/onLoad.R
@@ -54,24 +54,3 @@
 	
 	# packageStartupMessage or cat for furter information (package incompatibilities / updates)
 }  
-
-# TODO Remove in gMCP 0.8-1 and use loadJars from CommonJavaJars then.
-loadJars <- function(jars, java="J5") {
-	jarsFullname <- c()	
-	classes <- system.file("java", package = "CommonJavaJars", lib.loc = NULL)
-	files <- list.files(classes, full.names = FALSE)
-	if (java=="J5") {
-		files <- grep("J6", files, TRUE, value = TRUE, invert = TRUE)
-	}
-	for (j in jars) {
-		# Always take the newest jar per default:
-		x <- sort(grep(j, files, TRUE, value = TRUE), decreasing = TRUE)
-		if (length(x)==0) {
-			stop(paste("No jar that matches \"",j,"\" could be found.",sep=""))
-		}
-		jarsFullname <- c(jarsFullname, x[1])
-	}
-	
-	.jpackage("CommonJavaJars", jars=jarsFullname)
-	return(invisible(jarsFullname))
-}
