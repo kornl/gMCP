9c41255e6217fd31b7440fd067c1002751fac9d5
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2013-04-04 12:24:58 +0000

    Load REngine and JRIEngine from CommonJavaJars when rJava is > 0.9-3.

--- pkg/gMCP/R/onLoad.R
+++ pkg/gMCP/R/onLoad.R
@@ -33,6 +33,17 @@
 			}		
 		}
 	}
+	# If we have a rJava version > 0.9-3 load JRIEngine.jar and REngine.jar
+    if (!is.null(sessionInfo()$otherPkgs$rJava$Version) && sessionInfo()$otherPkgs$rJava$Version > "0.9-3") {
+		classes <- system.file("JRI", package = "CommonJavaJars", lib.loc = NULL)
+		if (nzchar(classes)) {
+			.jaddClassPath(classes)
+			jars <- grep(".*\\.jar", list.files(classes, full.names = TRUE), TRUE, value = TRUE)
+			if (length(jars)) { 
+				.jaddClassPath(jars)
+			}		
+		}
+	}
 	
 	## We supply our own JavaGD class
 	Sys.setenv("JAVAGD_CLASS_NAME"="org/mutoss/gui/JavaGD")  
