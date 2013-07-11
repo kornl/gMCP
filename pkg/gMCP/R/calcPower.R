kornl/gMCP

commit acaee497d59b343d8ac361af1c7c704c58cea436

Fixes #3. List of user defined power functions doesn't need to have names. If the list has no names, the functions will be referenced as "func1", "func2", etc. in the output.

pkg/gMCP/R/calcPower.R
@@ -5,7 +5,12 @@ extractPower <- function(x, f=list()) {
   allPow <- mean(rowSums(x)==dim(x)[2])
   result <- list(LocalPower = pow, ExpRejections = avgPow,
 		  PowAtlst1 = atleast1, RejectAll = allPow)
-  if(length(unique(names(f)))!=length(f)) stop("f must be a named list with unique names.")
+  if (is.function(f)) {f <- list(f)}
+  n <- names(f)
+  if (is.null(n) || all(is.na(n))) n <- paste("func", 1:length(f), sep="")
+  n[n=="" | is.na(n)] <- paste("func", 1:sum(n==""), sep="")
+  n <- make.names(n, unique=TRUE)
+  names(f) <- n
   for (fn in names(f)) {
   	result[[fn]] <- sum(apply(x,1, f[[fn]]))/dim(x)[1]
   }

pkg/gMCP/TODO
@@ -1,3 +1,4 @@
+- http://semver.org/ Should we switch from version numbers like 0.8-3 to 0.8.3?
 - NICER BUGREPORT!
 - Let user choose colors for entangled graphs.
 - Simes + parametric test for entangled graphs.

pkg/gMCP/man/calcPower.Rd
@@ -32,9 +32,11 @@
   \item{nSim}{ Monte Carlo sample size. If type = "quasirandom" this
     number is rounded up to the next power of 2, e.g. 1000 is rounded up
     to \eqn{1024=2^10}{1024=2^10} and at least 1024.}
-  \item{f}{ List of user defined power functions. If one is interested
-    in the power to reject hypotheses 1 and 3 one could specify
-    \code{f=list(function(x) {x[1] && x[3]})}
+  \item{f}{ List of user defined power functions (or just a single power function).
+   	If one is interested in the power to reject hypotheses 1 and 3 one could specify
+    \code{f=function(x) {x[1] && x[3]}}. If the power of rejecting hypotheses 1 and 2 is also of interest
+    one would use a (optionally named) list: \code{f=list(power1and3=function(x) {x[1] && x[3]}, power1and2=function(x) {x[1] && x[2]})}.
+    If the list has no names, the functions will be referenced as "func1", "func2", etc. in the output.
   }  
   \item{test}{ In the parametric case there is more than one way to handle 
     subgraphs with less than the full alpha. If the parameter \code{test} is missing,

commit dccd702d05ef6e4bf549399562747033f4a01898

Fixes regression introduced with commit acaee497 if f is missing, i.e. has a default value =list().

pkg/gMCP/R/calcPower.R
@@ -6,11 +6,13 @@ extractPower <- function(x, f=list()) {
   result <- list(LocalPower = pow, ExpRejections = avgPow,
 		  PowAtlst1 = atleast1, RejectAll = allPow)
   if (is.function(f)) {f <- list(f)}
-  n <- names(f)
-  if (is.null(n) || all(is.na(n))) n <- paste("func", 1:length(f), sep="")
-  n[n=="" | is.na(n)] <- paste("func", 1:sum(n==""), sep="")
-  n <- make.names(n, unique=TRUE)
-  names(f) <- n
+  if (length(f)>0) {
+    n <- names(f)
+    if (is.null(n) || all(is.na(n))) n <- paste("func", 1:length(f), sep="")
+    n[n=="" | is.na(n)] <- paste("func", 1:sum(n==""), sep="")
+    n <- make.names(n, unique=TRUE)
+    names(f) <- n
+  }
   for (fn in names(f)) {
   	result[[fn]] <- sum(apply(x,1, f[[fn]]))/dim(x)[1]
   }
@@ -39,6 +41,9 @@ calcPower <- function(weights, alpha, G, mean = rep(0, nrow(sigma)),
 	  }
 	  return(result)
   } else {
+    print(mean)
+    print(sigma)
+    print(nSim)
 	  sims <- rqmvnorm(nSim, mean = mean, sigma = sigma, seed = seed, type = type)
 	  pvals <- pnorm(sims, lower.tail = FALSE)
 	  out <- graphTest(pvalues=pvals, weights=weights, alpha=alpha, G=G, cr=cr, test=test)