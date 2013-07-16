40cd0a4cd9b819efcdab5c1b2aed16e8171ffd75
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2013-07-16 22:24:11 +0200

    Fixed scripts so that they are now (hopefully) POSIX conform.

--- pkg/gMCP/vignettes/createSweaveVignette.sh
+++ pkg/gMCP/vignettes/createSweaveVignette.sh
@@ -1,7 +1,7 @@
 RNWFILE=$1
-TEXFILE=$(basename $RNWFILE .Rnw).tex
-if [ -n $R_HOME ]; then 
-  R_HOME=$(R RHOME)
+TEXFILE=`basename $RNWFILE .Rnw`.tex
+if [ -z $R_HOME ]; then 
+  R_HOME=`R RHOME`
 fi
 RPROG=$R_HOME/bin/R
 RSCRIPT=$R_HOME/bin/Rscript
