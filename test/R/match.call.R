8d4606cf133907a8df8d19e2eb34607237620224
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2014-03-02 09:26:53 +0100

    Results from a gMCP call now have an attribute 'call' containing a string (or do we want to convert later?) corresponding to the call that produced it (with fully specified argument names).

---
+++ test/R/match.call.R
@@ -0,0 +1,13 @@
+
+
+gMCP2 <- function(graph, pvalues, test, correlation, alpha=0.05, 
+                 approxEps=TRUE, eps=10^(-3), ..., useC=FALSE, 
+                 verbose=FALSE, keepWeights=TRUE, adjPValues=TRUE) {
+  result <- 1
+  return(match.call())
+  attr(result, "call") <- match.call()
+  return(result)
+}
+
+g <- BonferroniHolm(3)
+gMCP2(g, pvalues=c(0.01, 0.02, 0.04, 0.04, 0.7))
