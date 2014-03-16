72df26beabca338a77d5b59102af75affbb69c53
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2014-03-16 16:06:56 +0100

    Note about 'permutations' calculation time.

--- pkg/gMCP/R/generateWeights.R
+++ pkg/gMCP/R/generateWeights.R
@@ -48,6 +48,8 @@ function(g,w){
   t(m)
 }
 
+# Calculation time note: n=22 needs 12 seconds on my computer.
+# With each further step calculation time nearly doubles.
 permutations <- function(n) {
 	outer((1:(2^n))-1, (n:1)-1, FUN=function(x,y) {(x%/%2^y)%%2})
 }
