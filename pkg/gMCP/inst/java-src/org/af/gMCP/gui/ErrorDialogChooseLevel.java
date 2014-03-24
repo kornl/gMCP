23c7c9c5567380d027fe1f39d3d3a2e28716b482
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2014-03-24 13:03:27 +0100

    Further work on choosing report levels.

--- pkg/gMCP/inst/java-src/org/af/gMCP/gui/ErrorDialogChooseLevel.java
+++ pkg/gMCP/inst/java-src/org/af/gMCP/gui/ErrorDialogChooseLevel.java
@@ -3,12 +3,15 @@ package org.af.gMCP.gui;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JTextArea;
 
+import org.af.commons.widgets.buttons.OkCancelButtonPane;
 import org.af.gMCP.config.Configuration;
 
 import com.jgoodies.forms.layout.CellConstraints;
@@ -18,19 +21,20 @@ public class ErrorDialogChooseLevel extends JDialog implements ActionListener {
 
 	String[] reportLevels = new String[] {
 			"Send no error report.",
-			"Minimal: Just send the stack trace + version + OS type",
+			"Minimal: Just send the error + version + OS type",
 			"Default: Send the most important information",
 			"Maximal: Includes more information about the system"
 	};
 	
 	protected JComboBox jcbReportLevel;
-	protected JCheckBox jcbScreenshot = new JCheckBox("Send screenshot of GUI");	
+	protected JCheckBox jcbScreenshot = new JCheckBox("Send screenshot of gMCP GUI window");	
+	protected JLabel reviewInfo = new JLabel("You will be able to review and change the collected data.");
 	
 	public ErrorDialogChooseLevel(JFrame parent) {
 		super(parent, "Select information", true);
 		setLocationRelativeTo(parent);
 
-		String cols = "5dlu, pref, 5dlu, fill:pref:grow, 5dlu";
+		String cols = "5dlu, fill:pref:grow, 5dlu, pref, 5dlu";
 		String rows = "5dlu, pref, 5dlu, pref, 5dlu, pref, 5dlu, pref, 5dlu, pref, 5dlu, pref, 5dlu";
 
 		FormLayout layout = new FormLayout(cols, rows);
@@ -60,6 +64,8 @@ public class ErrorDialogChooseLevel extends JDialog implements ActionListener {
 		
 		getContentPane().add(jcbReportLevel, cc.xyw(2, row, 3));
 
+		row += 2;
+		
 		jlabel = new JTextArea("The following information will be send to us:");
 		jlabel.setOpaque(false);
 		jlabel.setEditable(false);
@@ -68,11 +74,17 @@ public class ErrorDialogChooseLevel extends JDialog implements ActionListener {
 		
 		row += 2;		
 
-		
+		getContentPane().add(reviewInfo, cc.xyw(2, row, 3));
 		
 		row += 2; 
 		
 		getContentPane().add(jcbScreenshot, cc.xyw(2, row, 3));
+
+		row += 2;
+		
+		OkCancelButtonPane ocPane = new OkCancelButtonPane();
+		getContentPane().add(ocPane, cc.xy(4, row));
+		ocPane.addActionListener(this);
 		
 		pack();
 		
@@ -82,5 +94,9 @@ public class ErrorDialogChooseLevel extends JDialog implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		Configuration.getInstance().setClassProperty(this.getClass(), "reportLevel", ""+jcbReportLevel.getSelectedIndex());
 	
+	}
+	
+	public static void main(String[] args) {
+		new ErrorDialogChooseLevel(new JFrame());
 	}
 }
