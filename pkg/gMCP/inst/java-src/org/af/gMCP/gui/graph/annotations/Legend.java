fe4ba842ecd44c9a41c937d7066d001bf1ffe56d
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2014-06-10 12:27:25 +0200

    New annotation `Legend`. Not sure whether and how this will be constucted by using the base classes.

---
+++ pkg/gMCP/inst/java-src/org/af/gMCP/gui/graph/annotations/Legend.java
@@ -0,0 +1,62 @@
+package org.af.gMCP.gui.graph.annotations;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Shape;
+import java.awt.font.GlyphVector;
+import java.awt.font.TextLayout;
+import java.awt.geom.AffineTransform;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Legend extends Annotation {
+
+	public Legend() {
+		
+	}
+	
+	public void paintLegend(Graphics graphics) {
+		Graphics2D g = (Graphics2D) graphics;
+		//GlyphVector gv = g.getFont().createGlyphVector(g.getFontRenderContext(), "Component graph 1");
+		//Shape shape = gv.getGlyphOutline(1);
+		 Font f = new Font("Helvetica", 1, 12);
+		 
+		 String s = new String("Component graph 1");
+		 TextLayout textLayout = new TextLayout(s, f, g.getFontRenderContext());		   
+		 Shape outline = textLayout.getOutline(null);
+		 
+		 AffineTransform transform = g.getTransform();		 
+		 transform.translate(100, 100);		 
+		 g.transform(transform);		 
+		 
+		 g.setColor(Color.BLUE);
+		 g.fill(outline);
+		 
+		 g.setColor(Color.BLACK);
+		 g.draw(outline);
+		 
+	}
+	
+	public static void main(String[] args) {
+		JFrame f = new JFrame();
+		f.setVisible(true);
+		f.setSize(800, 600);
+		f.setContentPane(new TestPanel());
+		Legend l = new Legend();
+		System.out.println(l.saveToR());
+	}
+	
+}
+
+class TestPanel extends JPanel {
+	
+	Legend l = new Legend();
+	
+	public void paintComponent(Graphics g) {
+		l.paintLegend(g);
+	}
+	
+}
