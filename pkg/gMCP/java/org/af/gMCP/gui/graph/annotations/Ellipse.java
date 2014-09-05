088520af277ed9546e447f592d4faf57b74dec97
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2014-09-06 00:52:56 +0200

    Mathematics whether a given point is inside an ellipse or not (given by (x,y,width,height)).

--- pkg/gMCP/java/org/af/gMCP/gui/graph/annotations/Ellipse.java
+++ pkg/gMCP/java/org/af/gMCP/gui/graph/annotations/Ellipse.java
@@ -1,7 +1,9 @@
 package org.af.gMCP.gui.graph.annotations;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 import javax.json.stream.JsonGenerator;
 
@@ -9,12 +11,14 @@ public class Ellipse extends Annotation {
 
 	int width;
 	int height;
+	Color color = Color.BLUE;
 	
-	public Ellipse(int x, int y, int width, int height) {
+	public Ellipse(int x, int y, int width, int height, AnnotationPanel nl) {
 		this.x = x;
 		this.y = y;
 		this.width = width;
 		this.height = height;
+		this.nl = nl;
 	}
 
 	@Override
@@ -25,7 +29,9 @@ public class Ellipse extends Annotation {
 
 	@Override
 	public Dimension paintObject(Graphics graphics) {
-		// TODO Auto-generated method stub
+		Graphics2D g = (Graphics2D) graphics;
+		g.setColor(color);
+		g.drawOval((int)(x*nl.getZoom()), (int)(y*nl.getZoom()), (int)(width*nl.getZoom()), (int)(height*nl.getZoom()));		
 		return new Dimension(width, height);
 	}
 
@@ -42,8 +48,30 @@ public class Ellipse extends Annotation {
 	}
 
 	@Override
-	public boolean inYou(int x, int y) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean inYou(int xP, int yP) {
+		double m1 = x + width/2.0;
+		double m2 = y + height/2.0;
+		boolean value = false;
+		if (width==height) {
+			value = (Math.sqrt((xP-m1)*(xP-m1)+(yP-m2)*(yP-m2))<width/2);
+		} else if (width>height) {
+			double f11 = x+width/2-Math.sqrt(width*width/4-height*height/4);
+			double f21 = x+width/2+Math.sqrt(width*width/4-height*height/4);
+			double f12 = y+height/2;
+			double f22 = y+height/2;
+			value = Math.sqrt((xP-f11)*(xP-f11)+(yP-f12)*(yP-f12))+Math.sqrt((xP-f21)*(xP-f21)+(yP-f22)*(yP-f22))<width;
+		} else {
+			double f11 = x+width/2;
+			double f21 = x+width/2;
+			double f12 = y+height/2-Math.sqrt(height*height/4-width*width/4);
+			double f22 = y+height/2+Math.sqrt(height*height/4-width*width/4);
+			value = Math.sqrt((xP-f11)*(xP-f11)+(yP-f12)*(yP-f12))+Math.sqrt((xP-f21)*(xP-f21)+(yP-f22)*(yP-f22))<height;
+		}
+		if (value) {
+			color = Color.GREEN;
+		} else {
+			color = Color.RED;
+		}
+		return value;
 	}
 }
