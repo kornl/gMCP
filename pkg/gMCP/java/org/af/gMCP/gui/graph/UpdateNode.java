f596061c25a46d215b3955620441a465def7744c
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2015-05-03 14:26:13 +0200

    Update node dialogs can second tab for node color and (global) radius.

--- pkg/gMCP/java/org/af/gMCP/gui/graph/UpdateNode.java
+++ pkg/gMCP/java/org/af/gMCP/gui/graph/UpdateNode.java
@@ -10,9 +10,15 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JSpinner;
+import javax.swing.JTabbedPane;
 import javax.swing.JTextField;
+import javax.swing.SpinnerNumberModel;
 
+import org.af.gMCP.config.Configuration;
 import org.af.gMCP.gui.RControl;
+import org.af.gMCP.gui.dialogs.ColorChooseDialog;
 import org.apache.commons.lang.ArrayUtils;
 
 import com.jgoodies.forms.layout.CellConstraints;
@@ -27,6 +33,9 @@ public class UpdateNode extends JDialog implements ActionListener {
 	Node node;
 	NetList netzListe;
 	GraphView gv;
+	JTabbedPane tabbedPane = new JTabbedPane();
+	JSpinner spinner;
+	JButton jbColor = new JButton("Choose Color");
 	
 	public UpdateNode(Node node, GraphView gv) {
 		super(gv.parent, "Updating Node "+node.getName(), true);
@@ -34,36 +43,19 @@ public class UpdateNode extends JDialog implements ActionListener {
 		this.gv = gv;
 		this.netzListe = gv.getNL();
 		String cols = "5dlu, fill:pref:grow, 5dlu, fill:pref:grow, 5dlu";
-        String rows = "5dlu, pref, 5dlu, pref, 5dlu";
-        for (double w : node.getWeight()) {
-        	rows += ", pref, 5dlu";
-        }
+        String rows = "5dlu, fill:pref:grow, 5dlu, pref, 5dlu";
+                
+        spinner = new JSpinner(new SpinnerNumberModel(node.getRadius(), 1, 100, 1));
         
         FormLayout layout = new FormLayout(cols, rows);
         getContentPane().setLayout(layout);
         CellConstraints cc = new CellConstraints();
-		
-        int row = 2;
         
-        JTextField tf;
-        for (double w : node.getWeight()) {
-        	getContentPane().add(new JLabel("Weight for node "+node.getName()), cc.xy(2, row));
-        	tf = new JTextField("", 7);
-        	tf.setText(""+RControl.getFraction(w));
-        	tf.addActionListener(this);
-        	tfList.add(tf);
-        	getContentPane().add(tf, cc.xy(4, row));
-        	row += 2;
-        }
-        
-        getContentPane().add(new JLabel("New name"), cc.xy(2, row));
+        tabbedPane.addTab("Weights & Names", getMainPanel());
+        tabbedPane.addTab("Further Attributes", getSubPanel());
+        getContentPane().add(tabbedPane, cc.xyw(1, 2, 5));
 
-        tfname = new JTextField();
-        tfname.addActionListener(this);
-        tfname.setText(node.getName());
-        getContentPane().add(tfname, cc.xy(4, row));
-
-        row += 2;
+		int row = 4;
         
         jbDeleteNode.addActionListener(this);
         getContentPane().add(jbDeleteNode, cc.xy(2, row));
@@ -72,11 +64,85 @@ public class UpdateNode extends JDialog implements ActionListener {
         getContentPane().add(jb, cc.xy(4, row));
         
         pack();
+        setSize(Math.max(this.getSize().width, 450), this.getSize().height);
         this.setLocation(300, 300);
         setVisible(true);
 	}
+	
+	protected JPanel getMainPanel() {
+		JPanel panel = new JPanel();
+		
+		String cols = "5dlu, fill:pref:grow, 5dlu, fill:pref:grow, 5dlu";
+        String rows = "5dlu, pref, 5dlu, pref, 5dlu";
+        
+        for (double w : node.getWeight()) {
+        	rows += ", pref, 5dlu";
+        }
+        
+        FormLayout layout = new FormLayout(cols, rows);
+        panel.setLayout(layout);
+        CellConstraints cc = new CellConstraints();
+		
+        int row = 2;
+        
+        JTextField tf;
+        for (double w : node.getWeight()) {
+        	panel.add(new JLabel("Weight for node "+node.getName()), cc.xy(2, row));
+        	tf = new JTextField("", 7);
+        	tf.setText(""+RControl.getFraction(w));
+        	tf.addActionListener(this);
+        	tfList.add(tf);
+        	panel.add(tf, cc.xy(4, row));
+        	row += 2;
+        }
+        
+        panel.add(new JLabel("New name"), cc.xy(2, row));
+
+        tfname = new JTextField();
+        tfname.addActionListener(this);
+        tfname.setText(node.getName());
+        panel.add(tfname, cc.xy(4, row));
+        
+		return panel;
+	}
+
+	JLabel colorLabel = new JLabel("     ");
+	
+	protected JPanel getSubPanel() {
+		JPanel panel = new JPanel();
+		String cols = "5dlu, fill:pref:grow, 5dlu, fill:pref:grow, 5dlu, fill:pref:grow, 5dlu";
+        String rows = "5dlu, pref, 5dlu, pref, 5dlu";
+        
+        FormLayout layout = new FormLayout(cols, rows);
+        panel.setLayout(layout);
+        CellConstraints cc = new CellConstraints();
+        
+        int row = 2;
+        panel.add(new JLabel("Color:"), cc.xy(2, row));
+		
+        colorLabel.setOpaque(true);
+        colorLabel.setBackground(node.getColor());
+        panel.add(colorLabel, cc.xy(4, row));
+        
+        jbColor.addActionListener(this);
+        panel.add(jbColor, cc.xy(6, row));
+        
+        row += 2;
+        
+        panel.add(new JLabel("Radius (for all nodes):"), cc.xy(2, row));
+		
+        panel.add(spinner, cc.xy(4, row));
+        
+		return panel;
+	}
 
 	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == jbColor) {
+			ColorChooseDialog ccd = new ColorChooseDialog(this);
+			node.setColor(ccd.getColor());
+			colorLabel.setBackground(node.getColor());
+			return;
+		}
 		if (e.getSource().equals(jbDeleteNode)) {
 			netzListe.removeNode(node);
 			dispose();		
@@ -95,6 +161,7 @@ public class UpdateNode extends JDialog implements ActionListener {
 			}
 		}
 		node.setWeight(ArrayUtils.toPrimitive((Double[])wList.toArray(new Double[0])), null);
+		node.setRadius(Integer.parseInt(spinner.getModel().getValue().toString()));
 		int which = netzListe.whichNode(tfname.getText());
 		if (which == -1 || netzListe.getNodes().get(which) == node) {
 			gv.renameNode(node, tfname.getText());			
