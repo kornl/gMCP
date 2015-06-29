0ae4001204262b2b4d5ab113c0538c4b58687f5a
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2015-06-30 01:19:33 +0200

    gMCP.extended basic workflow

--- pkg/gMCP/R/gMCP.extended.R
+++ pkg/gMCP/R/gMCP.extended.R
@@ -1,9 +1,37 @@
 graph <- BonferroniHolm(4)
 pvalues <- c(0.01, 0.05, 0.03, 0.02)
+alpha <- 0.05
 
 # Weighted Bonferroni-test
 test <- function(pvalues, weights, alpha) {
   return(any(pvalues<alpa*weights))
+}
+
+# Simes test
+test <- function(pvalues, weights, alpha) {
+  mJ <- Inf  				
+  for (j in J) {
+    Jj <- subset!=0 & (pvalues2 <= pvalues2[j]) # & (1:n)!=j
+    if (adjPValues) {
+      mJt <- pvalues2[j]/sum(weights[i, Jj])	
+      if (is.na(mJt)) { # this happens only if pvalues2[j] is 0
+        mJt <- 0
+      }
+      #cat("pvalues2:\n", pvalues2, "\nmJt: ", mJt, "\nmJ: ", mJ, "\nj: ", j, "\nJ: ", J, "\nsubset: ", subset, "\n")
+      if (mJt<mJ) {
+        mJ <- mJt
+      }
+    }
+    #cat("j: ",j, ", Jj: ",Jj,"\n")
+    #cat("p_",j,"=",pvalues2[j],"<=a*(w_",paste(which(Jj),collapse ="+w_"),")=",alpha,"*(",paste(weights[i, Jj],collapse ="+"),")=",sum(weights[i, Jj]),"\n")
+    if (pvalues2[j]<=alpha*sum(weights[i, Jj])) {
+      result[i, n+1] <- 1
+      if (verbose) {
+        explanation[i] <- paste("Subset {",paste(J,collapse=","),"}: p_",j,"=", pvalues2[j],"<=a*(w_",paste(which(Jj),collapse ="+w_"),")\n     =",alpha,"*(",paste(weights[i, Jj],collapse ="+"),")=",alpha*sum(weights[i, Jj]),sep="")
+      }
+    }
+  }	
+  result[i, n+2] <- mJ
 }
 
 # attach(loadNamespace("gMCP"), name="namespace:gMCP", pos=3)
@@ -25,6 +53,7 @@ gMCP.extended <- function(graph, pvalues, test, correlation, alpha=0.05,
   allSubsets <- permutations(length(getNodes(graph2)))[-1,]
   result <- cbind(allSubsets, 0, Inf)
   n <- length(graph@weights)
+  # Allow for different generateWeights? generateWeights can handle entangled graphs.
   weights <- generateWeights(graph2@m, getWeights(graph2))[,(n+(1:n))]
   
   if (verbose) explanation <- rep("not rejected", dim(allSubsets)[1])
@@ -32,38 +61,21 @@ gMCP.extended <- function(graph, pvalues, test, correlation, alpha=0.05,
     subset <- allSubsets[i,]
     if(!all(subset==0)) {
       J <- which(subset!=0)	
-      
-      
-      
-      
-      
-      
-      
-      if (verbose) explanation[i] <- paste("Subset {",paste(J,collapse=","),"}: ",explanation[i], sep="")			
-      mJ <- Inf					
-      for (j in J) {
-        Jj <- subset!=0 & (pvalues2 <= pvalues2[j]) # & (1:n)!=j
+      test.result <- test(pvalues[J], weights[1, J], alpha)
+      if (test.result) {
+        if (verbose) explanation[i] <- paste("Subset {",paste(J,collapse=","),"}: ",explanation[i], sep="") 
+      } else {
+        rejected <- "rejected"
+        if (verbose) {
+          expl <- attr(test.result, "explanation")
+          if(!is.null(expl)) explanation <- expl
+        } 
         if (adjPValues) {
-          mJt <- pvalues2[j]/sum(weights[i, Jj])	
-          if (is.na(mJt)) { # this happens only if pvalues2[j] is 0
-            mJt <- 0
-          }
-          #cat("pvalues2:\n", pvalues2, "\nmJt: ", mJt, "\nmJ: ", mJ, "\nj: ", j, "\nJ: ", J, "\nsubset: ", subset, "\n")
-          if (mJt<mJ) {
-            mJ <- mJt
-          }
-        }
-        #cat("j: ",j, ", Jj: ",Jj,"\n")
-        #cat("p_",j,"=",pvalues2[j],"<=a*(w_",paste(which(Jj),collapse ="+w_"),")=",alpha,"*(",paste(weights[i, Jj],collapse ="+"),")=",sum(weights[i, Jj]),"\n")
-        if (pvalues2[j]<=alpha*sum(weights[i, Jj])) {
-          result[i, n+1] <- 1
-          if (verbose) {
-            explanation[i] <- paste("Subset {",paste(J,collapse=","),"}: p_",j,"=", pvalues2[j],"<=a*(w_",paste(which(Jj),collapse ="+w_"),")\n     =",alpha,"*(",paste(weights[i, Jj],collapse ="+"),")=",alpha*sum(weights[i, Jj]),sep="")
-          }
-        }
-      }	
-      result[i, n+2] <- mJ
-    } 
+          stat <- attr(test.result, "stat")
+        }          
+        if (verbose) explanation[i] <- paste("Subset {",paste(J,collapse=","),"}: ", rejected, sep="")
+      }
+    }
   }
   adjPValuesV <- rep(NA, n)
   for (i in 1:n) {
@@ -71,6 +83,6 @@ gMCP.extended <- function(graph, pvalues, test, correlation, alpha=0.05,
       graph <- rejectNode(graph, getNodes(graph2)[i])
     }
     adjPValuesV[i] <- max(result[result[,i]==1,n+2])
-  }
-  
+  }  
+  result <- new("gMCPResult", graphs=list(graph, alpha=alpha, pvalues=pvalues, rejected=getRejected(graph), adjPValues=adjPValuesV)
 }
