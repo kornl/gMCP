244aeea59cd2463d4a4027e6d4da11ef372b3f01
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2015-07-08 01:16:11 +0200

    Starting point for further sampSize tests.

---
+++ pkg/gMCP/inst/unitTests/runit.sampSize.R
@@ -0,0 +1,10 @@
+test.sampSize <- function() {
+  target <- 0.8
+  
+  graph <- BonferroniHolm(2)  
+  powerReqFunc <- function(x) { x[1] || x[2] }
+  result <- sampSize(graph, esf=c(1,1), effSize=c(1,1), powerReqFunc=powerReqFunc, target=target, alpha=0.05)
+  
+  result2 <- calcPower(graph=graph, alpha=0.05, mean = rep(sqrt(result$samp.size), 2), f=powerReqFunc)
+  checkTrue(result2[[5]]>target)
+}
