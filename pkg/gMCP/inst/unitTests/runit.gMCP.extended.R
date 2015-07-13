9c6c09e6bc433669714868a96f8e6a394d59a6c3
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2015-07-14 00:48:04 +0200

    New tests for gMCP.extended.

--- pkg/gMCP/inst/unitTests/runit.gMCP.extended.R
+++ pkg/gMCP/inst/unitTests/runit.gMCP.extended.R
@@ -6,16 +6,19 @@ test.gMCP.ext <- function() {
   # Bonferroni test:
   r1 <- gMCP.extended(graph=graph, pvalues=pvalues, test=bonferroni.test, verbose=TRUE)
   r2 <- gMCP(graph=graph, pvalues=pvalues, verbose=TRUE)
-  all.equal(r1@adjPValues, r2@adjPValues, check.attributes=FALSE) # No names for gMCP.extended
+  checkTrue(all.equal(r1@adjPValues, r2@adjPValues, check.attributes=FALSE)) # No names for gMCP.extended
   
   # Simes test:
   r1 <- gMCP.extended(graph=graph, pvalues=pvalues, test=simes.test, verbose=TRUE)
   r2 <- gMCP(graph=graph, pvalues=pvalues, test="Simes", verbose=TRUE)
-  all.equal(r1@adjPValues, r2@adjPValues) # TODO Both don't have names
+  checkTrue(all.equal(r1@adjPValues, r2@adjPValues)) # TODO Both don't have names
   
   # Parametric test:
   
   # Trimmed Simes test:
+  r1 <- gMCP.extended(graph=graph, pvalues=pvalues, test=bonferroni.trimmed.simes.test, verbose=TRUE, alpha=0.05, adjPValues = FALSE)
+  r2 <- gMCP.extended(graph=graph, pvalues=pvalues, test=bonferroni.test, verbose=TRUE)
+  checkTrue(all(as.numeric(r1@rejected) >= as.numeric(r2@rejected)))
   
   # Simes with partition:
   
