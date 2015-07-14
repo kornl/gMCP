c830eb4d2c225394d469b6bd8e87b534d1bfc970
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2015-07-15 01:08:40 +0200

    We use a local copy of XMLIO till these changes are available in afcommons.

--- pkg/gMCP/java/org/af/gMCP/gui/power/SettingsToXML.java
+++ pkg/gMCP/java/org/af/gMCP/gui/power/SettingsToXML.java
@@ -8,7 +8,6 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
-import org.af.commons.io.XMLIO;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.w3c.dom.Document;
