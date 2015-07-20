62f1afbd8a2f26f7e981fba9d0ac2c8b7a8e7abb
Kornelius Rohmeyer <rohmeyer@small-projects.de>
2015-07-20 13:53:54 +0200

    Further sampSize tests.

--- pkg/gMCP/inst/unitTests/runit.sampSize.R
+++ pkg/gMCP/inst/unitTests/runit.sampSize.R
@@ -7,4 +7,18 @@ test.sampSize <- function() {
   
   result2 <- calcPower(graph=graph, alpha=0.05, mean = rep(sqrt(result[[1]]$samp.size), 2), f=powerReqFunc)
   checkTrue(result2[[5]]>target)
+  
+  ####
+  
+  graph <- BonferroniHolm(4)
+  
+  powerReqFunc=list('all(x[c(1,2)]'=function(x) {all(x[c(1,2)])},
+                    'any(x[c(0,1)]'=function(x) {any(x[c(0,1)])})
+  
+  sampSize(graph=graph, 
+           effSize=list("Scenario 1"=c(1, 0.1, 0.1, 0.1), 
+                        "Scenario 2"=c(0.1, 2, 0.1, 0.1)), 
+           esf=c(0.5, 0.707, 0.5, 0.707),
+           powerReqFunc=powerReqFunc, 
+           corr.sim=diag(4), target=c(0.8, 0.8), alpha=0.025, n.sim=100)
 }
